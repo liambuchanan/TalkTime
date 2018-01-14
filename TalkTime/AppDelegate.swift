8ca95b5745f71ca86f7e71c87a14b4f1e5a78e95
Liam Buchanan <liam.buchanan@gmail.com>
2018-01-14 11:20:16 -0500

    Initial Commit

---
+++ TalkTime/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TalkTime
+//
+//  Created by Liam Buchanan on 1/14/18.
+//  Copyright © 2018 Liam Buchanan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
